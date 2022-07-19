c36e2e8dc75d6673e5911e8d71c3146d6181c054
MBrosik <michal.bros1@gmail.com>
2022-07-19 23:21:20 +0200

    nice commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# IRC
+
+## About
+School project whose main goal was to recreate the online chat room called IRC
+
+## Interface
+
+## Installation
+
+## Features
+
+
+## Technologies
---
+++ db/irc.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 19 Lip 2022, 23:20
+-- Wersja serwera: 10.4.20-MariaDB
+-- Wersja PHP: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `irc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `message` text COLLATE utf8_polish_ci NOT NULL,
+  `user` varchar(20) COLLATE utf8_polish_ci NOT NULL,
+  `color` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
+  `room` varchar(20) COLLATE utf8_polish_ci NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `messages`
+--
+
+INSERT INTO `messages` (`id`, `message`, `user`, `color`, `room`, `time`) VALUES
+(10123, 'Hello world', 'test1', '#3C48E5', 'Public', '2022-07-19 21:19:07');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10124;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
